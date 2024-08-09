a16e8f0310b80e76addf460c56bf991b47d312fb
Glorypixel <ifeomaglory1998@gmail.com>
2024-08-09 06:12:56 +0100

    i have added the product

--- src/components/products/products.tsx
+++ src/components/products/products.tsx
@@ -1,21 +1,23 @@
 import React from "react";
 import { Product, products } from "../../productList";
 import Image from "next/image";
+
 const Products: React.FC = () => {
   return (
-    <div>
-      <div className="flex justify-between gap-8 p-4">
-        <div className="p-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div className="m-16">
+      <div className="flex gap-8">
+        <div className="flex-1 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2">
           {products.map((product: Product) => (
-            <div
-              key={product.id}
-              className="border rounded-lg overflow-hidden shadow-lg"
-            >
-              <Image
-                src={product.imageURL}
-                alt={product.name}
-                className="w-full h-48 object-cover"
-              />
+            <div key={product.id}>
+              <div className="border rounded-lg overflow-hidden shadow-lg w-52 h-54">
+                <Image
+                  src={product.imageURL}
+                  alt={product.name}
+                  className="w-full h-48 object-cover"
+                //   width={200}
+                />
+              </div>
+
               <div className="p-4">
                 <h2 className="text-xl font-semibold mb-2">{product.name}</h2>
                 <p className="text-lg text-green-500 mb-2">
@@ -26,14 +28,12 @@ const Products: React.FC = () => {
             </div>
           ))}
         </div>
-
-        <div className="w-72 h-46 bg-gray-300 flex items-center justify-center">
+        <div className="w-72 h-48 bg-gray-300 flex items-center justify-center">
           Second Container
         </div>
       </div>
     </div>
   );
 };
-console.log(products); // Should output an array of Product objects
 
 export default Products;
